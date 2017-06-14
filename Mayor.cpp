4fbdc229a3fefa698a8351f14c0befa4b5886d1d
jtallacagua <tallacaguajesicaet5@gmail.com>
2017-06-13 23:12:37 -0300

    Create Mayor.cpp

---
+++ Mayor.cpp
@@ -0,0 +1,33 @@
+/* Trabajo #2 — Mayor de dos Numeros
+ Jesica Patrica Tallacagua Calle
+ 2017/06/13
+*/ 
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	double a,b;
+	cout<<"ingrese un numero"<<endl;
+	cin>>a;
+	cout<<"ingrese otro numero"<<endl;
+	cin>>b;
+	if (a>b)
+	{
+	  cout<<"el mas grande es "<<endl;
+	  cout<<a;
+	  
+	}
+	else  
+	{
+	  cout<<"el mas grande es "<< endl;
+	  cout<<b;
+	  	
+	}
+	
+	
+}
